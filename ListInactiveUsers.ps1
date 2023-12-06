3d0099c12bdfc62b5bc32e9e074d7c71a5e7d538
ACloudLab <106700753+ACloudLab@users.noreply.github.com>
2023-12-06 13:43:45 -0500

    Add files via upload

---
+++ ListInactiveUsers.ps1
@@ -0,0 +1,18 @@
+﻿Install-Module ImportExcel -AllowClobber -Force
+Get-Module ImportExcel -ListAvailable | Import-Module -Force -Verbose
+Set-executionpolicy bypass
+Install-Module AzureAD
+Install-Module AzureADPreview -AllowClobber -Force
+install-module msgraph
+Install-Module Microsoft.Graph -Scope CurrentUser -Force
+Import-Module Microsoft.Graph.Users
+Connect-MgGraph
+
+$inactiveDate = (Get-Date).AddDays(-30)
+
+$users = Get-MgUser -All:$true -Property Id,DisplayName,UserPrincipalName,UserType, SigninActivity, AccountEnabled
+$inactiveUsers = $users | Where-Object {
+    $_.SignInActivity.LastSignInDateTime -lt $inactiveDate
+} | Select-Object DisplayName, UserPrincipalName, UserType, AccountEnabled
+
+$inactiveUsers | Export-Excel -Path C:\Temp\InactiveUsers.xlsx
