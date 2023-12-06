6e48e7d0d1d011673261d3c89eef5c9dd52b3d89
ACloudLab <106700753+ACloudLab@users.noreply.github.com>
2023-12-06 13:19:53 -0500

    Add files via upload

---
+++ CreateSelfSignedCertificate.ps1
@@ -0,0 +1,4 @@
+﻿$cert = New-SelfSignedCertificate -FriendlyName "mysite.com" -DnsName mysite.com -CertStoreLocation "cert:\LocalMachine\My" -KeyUsage DigitalSignature,KeyEncipherment,DataEncipherment -KeyAlgorithm RSA -HashAlgorithm SHA256 -KeyLength 2048 -KeyUsageProperty All -Provider "Microsoft Enhanced RSA and AES Cryptographic Provider" -NotAfter (Get-Date).AddYears(5)
+$pass ="myCertPassword"
+$SecuredPass = ConvertTo-SecureString -String $pass -Force -AsPlainText
+Export-PfxCertificate -Cert "Cert:\LocalMachine\My\$($cert.Thumbprint)" -FilePath custom_cert.pfx -Password $SecuredPass
